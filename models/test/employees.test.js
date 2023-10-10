b32800b642b379ad7f43bb41017539e5fd65e35c
Dariusz Biernat <kaiju83@outlook.com>
2023-10-10 21:25:21 +0200

    Add test for department.model and employees.model

---
+++ models/test/employees.test.js
@@ -0,0 +1,54 @@
+const Employee = require('../employees.model');
+ const expect = require('chai').expect;
+ const mongoose = require('mongoose');
+
+
+ describe('Employee', () => {
+
+   it('should throw an error if no arg', () => {
+
+
+     const dep = new Employee({}); // create new Employee,
+
+     dep.validateSync(err => {
+       expect(err.errors.firstName).to.exist;
+     });
+     after(() => {
+       mongoose.models = {};
+     });
+   });
+
+   it('should throw an error if "firstName" is not a string', () => {
+
+     const cases = [{}, []];
+     for (let name of cases) {
+       const dep = new Employee({ name });
+
+       dep.validateSync(err => {
+         expect(err.errors.firstName).to.exist;
+       });
+     }
+     after(() => {
+       mongoose.models = {};
+     });
+   });
+   it('should not throw an error if "firstName", "lastName" and "department" is okay', () => {
+
+     const cases = [
+       { firstName: 'John', lastName: 'Doe', department: 'Tech' },
+       { firstName: 'Amanda', lastName: 'Watson', department: 'Marketing' },
+     ];
+     for (let name of cases) {
+       const dep = new Employee(name);
+
+       dep.validateSync(err => {
+         expect(err).to.not.exist;
+       });
+
+       after(() => {
+         mongoose.models = {};
+       });
+     }
+   });
+
+ });
